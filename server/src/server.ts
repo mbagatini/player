e2edd7a8879ea00dcfe54b2728886786b51ae497
Morgana <morganabagatini@gmail.com>
2023-04-06 09:34:30 -0300

    chore: adding cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { appRoutes } from './http/routes'
 import { env } from './env'
 import { errorHandler } from './http/utils/errorHandler'
@@ -6,6 +7,7 @@ import { errorHandler } from './http/utils/errorHandler'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(appRoutes)
 app.use(errorHandler)
