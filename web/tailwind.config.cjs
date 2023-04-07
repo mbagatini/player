3f47b2be3a58737657106ea71f2bb19b299b2758
Morgana <morganabagatini@gmail.com>
2023-04-07 15:07:22 -0300

    feat: change font style

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,6 +2,9 @@
 export default {
     content: ['./src/**/*.tsx'],
     theme: {
+        fontFamily: {
+            sans: ['Lexend', 'sans-serif'],
+        },
         extend: {},
     },
     plugins: [],
